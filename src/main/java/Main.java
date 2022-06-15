12304a7fa0beb0204f59e530e88afaa0b57ee2ef
RicoKoelling <rico.koelling@uni-jena.de>
2022-06-15 23:46:15 +0200

    init commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int i=0;
+        while (5 > i){
+            i++;
+            System.out.println("YOur mum * " + i);
+        }
+    }
+}
